c19e601a3008156f79975d8ea64b84b8d669878f
kaaku <92543349+sankaaku@users.noreply.github.com>
2021-12-04 13:18:54 -0800

    Add files via upload
    Even less messy

--- Main.cs
+++ Main.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using e = System.Collections.IEnumerator;
+using o = UnityEngine.Object;
+using c = UnityEngine.Camera;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(FOVCV1BK), "FOV Changer", "1", "kaaku")]
@@ -7,16 +8,16 @@ using MelonLoader;
 
 class FOVCV1BK : MelonMod
 {
-    static IEnumerator G()
+    static e G()
     {
         while (RoomManager.field_Internal_Static_ApiWorld_0 == null) yield return null;
-        foreach (var c in Object.FindObjectsOfType<Camera>())
-            if (c.name == "Camera (eye)") c.fieldOfView = FOV.Value;
-            else if (c.name == "StackedCamera : Cam_InternalUI") c.fieldOfView = FOVUI.Value;
+        foreach (var c in o.FindObjectsOfType<c>())
+            if (c.name == "Camera (eye)") c.fieldOfView = f.Value;
+            else if (c.name == "StackedCamera : Cam_InternalUI") c.fieldOfView = u.Value;
     }
     public override void OnApplicationStart() { MelonCoroutines.Start(G()); }
     public override void OnPreferencesSaved() { MelonCoroutines.Start(G()); }
     public override void OnPreferencesLoaded() { MelonCoroutines.Start(G()); }
-    static MelonPreferences_Category FOVC = MelonPreferences.CreateCategory("FOVC", "FOV Changer");
-    static MelonPreferences_Entry<float> FOV = FOVC.CreateEntry<float>("FOV", 60, "Field of View"), FOVUI = FOVC.CreateEntry<float>("FOVUI", 60, "UI Field of View");
+    static MelonPreferences_Category c = MelonPreferences.CreateCategory("FOVC", "FOV Changer");
+    static MelonPreferences_Entry<float> f = c.CreateEntry<float>("FOV", 60, "Field of View"), u = c.CreateEntry<float>("FOVUI", 60, "UI Field of View");
 }
